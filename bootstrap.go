7fb17fb50a9110e33f70ef3d5927c182f201ac01
hzwy23 <hzwy23@163.com>
2017-11-25 23:53:50 +0800

    add startup with config file

--- bootstrap.go
+++ bootstrap.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"strings"
+	"github.com/hzwy23/panda/config"
 	"github.com/hzwy23/panda/route"
 	"net/http"
 	"sync"
@@ -15,6 +17,20 @@ type RegisterFunc func()
 // value 注册路由方法
 var regApp = make(map[string]RegisterFunc)
 var regLock = new(sync.RWMutex)
+
+type WebApp struct{
+	Port        string
+	HttpsEnable bool
+	HttpsCrt    string
+	HttpsKey    string
+}
+
+var AppConfig = &WebApp{
+	Port:":8080",
+	HttpsEnable:false,
+	HttpsCrt:"./conf/rui.crt",
+	HttpsKey:"./conf/rui.key",
+}
 
 func AppRegister(name string, registerFunc RegisterFunc) {
 	regLock.Lock()
@@ -48,5 +64,54 @@ func Bootstrap() {
 	middle := route.NewMiddleware(authFilter, loggerFilter, route.Wrap(route.DefaultRouter()))
 
 	// 启动服务
-	http.ListenAndServe(":8080", middle)
+	// 从配置文件获取端口号
+	c,err := config.Load("conf/app.conf",config.INI)
+	if err != nil {
+		logger.Warn("读取配置文件conf/app.conf文件失败，使用默认参数启动服务")
+		http.ListenAndServe(AppConfig.Port,middle)
+		return
+	}
+
+	// 从配置文件中读取端口号
+	port,err := c.Get("ServerPort")
+	if err != nil {
+		logger.Warn("http.port 不存在，使用默认端口启动服务")
+		http.ListenAndServe(AppConfig.Port,middle)
+		return
+	}
+	AppConfig.Port = port
+
+	// 检查是否开启https
+	httpsEnable,err := c.Get("https.enable")
+	if err != nil {
+		logger.Warn("http.port 不存在，使用默认端口启动服务")
+		http.ListenAndServe(AppConfig.Port,middle)
+		return
+	}
+	AppConfig.HttpsEnable = (strings.ToLower(httpsEnable)=="true")
+	// 使用http启动web服务
+	if !AppConfig.HttpsEnable {
+		logger.Info("start web server with http.")
+		http.ListenAndServe(":"+AppConfig.Port, middle)
+		return
+	}
+
+	// 使用https启动服务，读取crt文件地址
+	httpsCrt,err := c.Get("https.crt")
+	if err != nil {
+		logger.Warn("http.port 不存在，使用默认端口启动服务")
+	} else {
+		AppConfig.HttpsCrt = httpsCrt
+	}
+
+	// 使用https启动服务，读取key文件地址
+	httpsKey,err := c.Get("https.key")
+	if err != nil {
+		logger.Warn("http.port 不存在，使用默认端口启动服务")
+	} else {
+		AppConfig.HttpsKey = httpsKey
+	}
+
+	http.ListenAndServeTLS(":"+AppConfig.Port,AppConfig.HttpsCrt,AppConfig.HttpsKey,middle)
+	
 }
